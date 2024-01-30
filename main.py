478a90f47910715b03927c6fdaa05cf49ff0ab74
kobbrak447 <153929734+kobbrak447@users.noreply.github.com>
2024-01-30 17:53:36 +0530

    Update main.pyf

--- main.py
+++ main.py
@@ -30,8 +30,8 @@ import io
 #import pycurl
 
 bot = Client(
-     "bot",="6969961194:AAHg6UtYLwMavm0kUpLHJfOUDXmbRjbG8jQ")
-      api_id=api_id=,23621134)
+     "bot",="6969961194:AAHg6UtYLwMavm0kUpLHJfOUDXmbRjbG8jQ",
+      api_id=api_id=23621134,
       api_hash=api_hash, "3e49039179441fb424d90680ecffe365")
      AUTH_USERS=6960520819
 
