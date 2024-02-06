5199c2d70180d1cfa0fc8305c6940fc0271826d9
kobbrak447 <153929734+kobbrak447@users.noreply.github.com>
2024-02-06 08:47:17 +0530

    Update details.py

--- details.py
+++ details.py
@@ -1,4 +1,4 @@
-api_id = "23621134"
-api_hash = "3e49039179441fb424d90680ecffe365"
-bot_token = "6969961194:AAHg6UtYLwMavm0kUpLHJfOUDXmbRjbG8jQ" 
+api_id = "25747764"
+api_hash = "0668ea2b549fee61548f3392a0021aff"
+bot_token = "5981404906:AAEVQc7gl_cu9ar24F2gsRZkzXRjfsSxhQ4" 
 sudo_group = -1002021057745
